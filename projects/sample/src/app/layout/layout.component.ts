eae3c33b8fb13cdd629f69d1f0134954e0054196
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2020-11-16 19:45:35 -0700

    feat(demo): navigate to example anchor tags

--- projects/sample/src/app/layout/layout.component.ts
+++ projects/sample/src/app/layout/layout.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy {
+  private destroySubject = new Subject();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.route.fragment
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe((frag) => {
+        if (!!frag) {
+          this.scroll(frag);
+        } else {
+          (function smoothScroll(): void {
+            const sidenavEl = document.getElementsByTagName(
+              'mat-sidenav-content'
+            )[0] as HTMLElement;
+            if (!sidenavEl) {
+              return;
+            }
+            const currentScroll = sidenavEl.scrollTop;
 
-  ngOnInit(): void {
+            if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothScroll);
+              sidenavEl.scrollTo(0, currentScroll - currentScroll / 8);
+              window.location.hash = '#';
+            }
+          })();
+        }
+      });
   }
 
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.destroySubject.next();
+  }
+
+  scroll(id): void {
+    setTimeout(() => {
+      const el = document.getElementById(id);
+      el.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    });
+  }
 }
